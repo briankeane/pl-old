55b350fda322f21f8b30bdfd35e3218d849341ab
Brian Keane <lonesomewhistle@gmail.com>
2014-05-21 16:58:26 -0500

    fixed file inclusion

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
 
 
+  get 'songs/new'
+
+  get 'songs/create'
+
+  get 'songs/update'
+
+  get 'songs/edit'
+
+  get 'songs/destroy'
+
+  get 'songs/index'
+
+  get 'songs/show'
+
   get 'users/new'
 
   get 'users/create'
