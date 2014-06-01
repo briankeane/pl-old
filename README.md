c02f6cd00112ad9e4408d963ffcf65e681c6e040
Brian Keane <lonesomewhistle@gmail.com>
2014-06-01 13:30:16 -0500

    Commented out seedfile, cleaned up asset pipeline

---
+++ README.md
@@ -0,0 +1,10 @@
+# Ploayola
+[description]
+
+## Getting Started
+
+## Migrating databases
+
+```
+$ DB_ENV=test bundle exec rake db:migrate
+```
--- web/config/initializers/assets.rb
+++ web/config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.precompile += %w( jquery-1.11.1.js )
 Rails.application.config.assets.precompile += %w( jquery-ui-1.10.4.custom.css )
 Rails.application.config.assets.precompile += %w( jquery-ui-1.10.4.custom.min.js )
+Rails.application.config.assets.precompile += %w( playlist-editor.js )
