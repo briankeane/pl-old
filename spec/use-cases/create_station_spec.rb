3595535209d7451232f7f4506c1ce543aa1ce004
Brian Keane <lonesomewhistle@gmail.com>
2014-06-03 13:56:49 -0500

    fixed create_station for postgres

--- spec/use-cases/create_station_spec.rb
+++ spec/use-cases/create_station_spec.rb
@@ -11,7 +11,8 @@ module PL
 
     it "creates a station" do
       user = PL.db.create_user({ twitter: "Bob", password: "password" })
-      result = PL::CreateStation.run({ user_id: user.id, heavy: [], medium: [], light: [], seconds_of_commercial_per_hour: 300 })
+      song = PL.db.create_song({ title: 'Bar Lights', artist: 'Brian Keane', duration: 39000, album: 'Coming Home' })
+      result = PL::CreateStation.run({ user_id: user.id, heavy: [song], medium: [song], light: [song], seconds_of_commercial_per_hour: 300 })
       expect(result.success?).to eq(true)
       expect(result.station.id).to_not be_nil
     end
